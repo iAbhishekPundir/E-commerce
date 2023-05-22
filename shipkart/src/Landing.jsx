8a3339825529911b9814061fe50f4ccf9b682575
Abhishek <pundirabhishek987@gmail.com>
2023-05-22 22:54:49 +0530

    refactor: code

--- shipkart/src/Landing.jsx
+++ shipkart/src/Landing.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
 
 export const Landing = () => {
+  const getData = async () => {
+    try {
+      const creds = {
+        email: "adarshbalika@gmail.com",
+        password: "adarshbalika"
+      }
+      const response = await fetch("api/auth/login", {
+        method: 'POST',
+        body: JSON.stringify(creds)
+      });
+      
+      const fetchedData = await response.json();
+      const {encodedToken} = fetchedData;
+      localStorage.setItem("encodedToken", encodedToken);
+      console.log(fetchedData);
+    } catch (e) {
+      console.error(e)
+    }
+  }
   return (
-    <div>Landing Page for E-commerce</div>
+    <div onClick={getData}>Landing Page for E-commerce</div>
   )
 }
