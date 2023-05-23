a82674a95af8df4187d8c442c359302433228ac7
Abhishek <pundirabhishek987@gmail.com>
2023-05-23 22:48:33 +0530

    feature: wrapping App inside context

---
+++ shipkart/src/frontend/contexts/Data/ProductDataContext.jsx
@@ -0,0 +1,59 @@
+import React, { createContext, useEffect, useReducer } from 'react'
+import { productReducer, initialState } from '../../reducers/ProductReducer';
+export const ProductDataContext = createContext();
+
+export const ProductDataProvider = ( {children}) => {
+  
+    const [state, dispatch] = useReducer(productReducer, initialState);
+
+    const getCategories= async () => {
+        try {
+            const response = await fetch("/api/categories");
+            const {categories} = await response.json();
+            console.log(categories);
+            dispatch({type:"INITIALIZE_CATEGORIES", payload: categories})
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const getProducts = async () => {
+        try {
+            const response = await fetch("/api/products");
+            const {products} = await response.json();
+            console.log(products);
+            dispatch({type:"INITIALIZE_PRODUCTS", payload: products})
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const getCart = async () => {
+        try {
+            const response = await fetch("/api/user/cart", {
+                method:"GET",
+                headers:{
+                    authorization:localStorage.getItem("encodedToken")
+                }
+            })
+            const data= await response.json();
+            console.log(data.cart);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getCategories();
+        getProducts();
+        getCart();
+    }, []);
+
+  return (
+    <>
+        <ProductDataContext.Provider value={{state}}>
+            {children}
+        </ProductDataContext.Provider>
+    </>
+  )
+}
--- shipkart/src/index.js
+++ shipkart/src/index.js
@@ -4,6 +4,17 @@ import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter as Router } from "react-router-dom";
+import {
+  ProductDataProvider,
+  ProductDataContext,
+} from "./frontend/contexts/Data/ProductDataContext";
+import {
+  AuthProvider,
+  AuthContext,
+} from "./frontend/contexts/Auth/AuthContext";
+
+export { ProductDataContext };
+export { AuthContext };
 
 // Call make Server
 makeServer();
@@ -11,7 +22,11 @@ makeServer();
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <ProductDataProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </ProductDataProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
