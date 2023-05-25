595df19ffc97d00aeeaef3248d1ae521793e6ff2
Abhishek <pundirabhishek987@gmail.com>
2023-05-25 07:28:45 +0530

    refactor: code

--- shipkart/src/frontend/reducers/ProductReducer.js
+++ shipkart/src/frontend/reducers/ProductReducer.js
@@ -1,32 +1,30 @@
 // import React from 'react'
 export const initialState = {
-    isLoading: true,
-    error: null,
-    categories: [],
-    products: [],
-    cart:[],
-    wishlist:[],
-    filters: {
-        searchValue: null,
-        sort: null,
-        selectedCategories: [],
-        selectedSizes: [],
-        rating: null,
-        price:null,
-        byStock:true
-    }
-}
+  isLoading: true,
+  error: null,
+  categories: [],
+  products: [],
+  cart: [],
+  wishlist: [],
+  filters: {
+    searchValue: null,
+    sort: null,
+    selectedCategories: [],
+    selectedSizes: [],
+    rating: null,
+    price: null,
+    byStock: true,
+  },
+};
 export const productReducer = (state, action) => {
-
-    switch(action.type) {
-
-        case "INITIALIZE_CATEGORIES":
-            console.log(action.payload);
-            return {...state, isLoading: false,  categories: action.payload};
-        case "INITIALIZE_PRODUCTS":
-            console.log(action.payload);
-            return {...state, isLoading: false, products: action.payload};
-        case "FETCH_ERROR":
-            return { ...state, isLoading: false, error: action.payload };
-    }
-}
+  switch (action.type) {
+    case "INITIALIZE_CATEGORIES":
+      console.log(action.payload);
+      return { ...state, isLoading: false, categories: action.payload };
+    case "INITIALIZE_PRODUCTS":
+      console.log(action.payload);
+      return { ...state, isLoading: false, products: action.payload };
+    case "FETCH_ERROR":
+      return { ...state, isLoading: false, error: action.payload };
+  }
+};
