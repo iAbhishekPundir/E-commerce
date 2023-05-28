18bd5d68bc3bc9592d11aae6101c27f6e76b1b92
Abhishek <pundirabhishek987@gmail.com>
2023-05-28 07:35:47 +0530

    feat: CategoryLisying Componenet

---
+++ shipkart/src/frontend/pages/CategoriesListing/CategoriesListing.jsx
@@ -0,0 +1,38 @@
+import React, { useContext } from "react";
+import { ProductDataContext } from "../../contexts/Data/ProductDataContext";
+import "./CategoriesListing.css";
+import { CategoryCard } from "../../components/CategoryCard/CategoryCard";
+import cover from "../CategoriesListing/cover.png";
+
+export const CategoriesListing = () => {
+  const { state } = useContext(ProductDataContext);
+  return (
+    <>
+      <section className="category-container ">
+        <img className="homeImg" src={cover} alt="cover" />
+      </section>
+      <section className="services-section">
+        <div className="services-div">
+          <p className="services">Free Shipping</p>
+        </div>
+        <div className="services-div">
+          <p className="services">Ease Return</p>
+        </div>
+        <div className="services-div">
+          <p className="services">Branded items</p>
+        </div>
+        <div className="services-div">
+          <p className="services">Support 24 * 7</p>
+        </div>
+      </section>
+        <div className="shop-by-category-div">
+            <p>SHOP BY CATEGORY</p>
+        </div>
+      <div className="category-card-container">
+        {state.categories.map((category) => (
+          <CategoryCard category={category} />
+        ))}
+      </div>
+    </>
+  );
+};
