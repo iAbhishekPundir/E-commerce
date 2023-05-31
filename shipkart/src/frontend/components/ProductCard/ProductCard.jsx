aa1ea724fced90209421fe384189dbc99427bc95
Abhishek <pundirabhishek987@gmail.com>
2023-05-31 21:16:22 +0530

    feature: ProductCard

--- shipkart/src/frontend/components/ProductCard/ProductCard.jsx
+++ shipkart/src/frontend/components/ProductCard/ProductCard.jsx
@@ -1,5 +1,10 @@
-import React from "react";
-import "./ProductCard.css";
+import {useContext} from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+import { ProductDataContext } from '../../contexts/Data/ProductDataContext';
 
 export const ProductCard = ({ product }) => {
   const {
@@ -18,13 +23,33 @@ export const ProductCard = ({ product }) => {
     delivery_time,
     category,
   } = product;
+  const { dispatch } = useContext(ProductDataContext);
   return (
     <>
-      <div className="child-container">
-        <div className="image-div"><img src={image} alt={title} /></div>
-        <p>{title}</p>
-        <p>{description}</p>
-      </div>
+     <Card sx={{ maxWidth: 200 }} key={id}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={image}
+          alt={title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h6" component="div">
+            {title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+           $ {price}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary" variant="outlined" 
+        >
+          Add to cart
+        </Button>
+      </CardActions>
+    </Card>
     </>
-  )
+  );
 };
