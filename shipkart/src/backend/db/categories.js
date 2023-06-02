64742fd062464d5feb2c4169553af9c1b3a68207
Abhishek <pundirabhishek987@gmail.com>
2023-06-02 21:51:08 +0530

    refactor: code

--- shipkart/src/backend/db/categories.js
+++ shipkart/src/backend/db/categories.js
@@ -1,5 +1,5 @@
 
-import { v4 as uuid } from 'uuid';
+// import { v4 as uuid } from 'uuid';
 
 /**
  * Category Database can be added here.
@@ -8,19 +8,19 @@ import { v4 as uuid } from 'uuid';
 
 export const categories = [
   {
-    _id: '68a20c94-47ec-43ad-be3e-d31722a2cee9',
+    id: '1',
     categoryName: 'Men',
     image:"https://rukminim1.flixcart.com/image/612/612/xif0q/shirt/d/o/6/s-denim-shirt-18plus-original-imaghfgmwh7mxzek.jpeg?q=70",
     description: 'Checkout best Mens collection',
   },
   {
-    _id: '7543d803-3418-42af-b384-dea65d601302',
+    id: '2',
     categoryName: 'Women',
     image:"https://images.pexels.com/photos/1536619/pexels-photo-1536619.jpeg?auto=compress&cs=tinysrgb&w=600",
     description: 'Checkout best Womens collection',
   },
   {
-    _id: '46c210cf-3efa-46b7-bdaa-73d001d7b6ab',
+    id: '3',
     categoryName: 'Kids',
     image:"https://images.pexels.com/photos/4715320/pexels-photo-4715320.jpeg?auto=compress&cs=tinysrgb&w=600",
     description: 'Checkout best Kids collection',
